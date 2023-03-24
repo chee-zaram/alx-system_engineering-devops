a84667ed5f0f92c1e2b8004276044f76d300b5ce
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2023-03-24 20:57:42 +0100

    perf: use `package` resource for `flask` installation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -10,8 +10,8 @@ package { 'python3-pip':
     require => Exec['update-apt'],
 }
 
-exec { 'install-flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0"$',
-    require => Package['python3-pip'],
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    require  => Package['python3-pip'],
 }
